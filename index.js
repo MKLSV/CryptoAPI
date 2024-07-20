ef1ebc4a88e25fe72c9a46d07c24f79a93539917
Matvey Kolosov <118886359+MKLSV@users.noreply.github.com>
2024-07-20 19:13:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function convertToCsv(data) {
     ]
   });
 
-  const records = data.result.list.map(entry => ({
+  const records = data.map(entry => ({
     date: new Date(parseInt(entry[0])).toISOString(),
     open: entry[1],
     high: entry[2],
@@ -35,30 +35,49 @@ function convertToCsv(data) {
   return csvStringifier.getHeaderString() + csvStringifier.stringifyRecords(records);
 }
 
+// Функция для получения данных с Bybit API с обработкой лимита в 200 записей
+async function fetchAllData(params) {
+  let allData = [];
+  let hasMore = true;
+  let endTime = params.end;
+
+  while (hasMore) {
+    const response = await axios.get('https://api.bybit.com/v5/market/kline', {
+      params: {
+        ...params,
+        end: endTime
+      }
+    });
+
+    if (response.data.result && response.data.result.list) {
+      const data = response.data.result.list;
+      allData = allData.concat(data);
+
+      if (data.length < 200) {
+        hasMore = false;
+      } else {
+        endTime = data[data.length - 1][0];
+      }
+    } else {
+      throw new Error('No data in response from Bybit API');
+    }
+  }
+
+  return allData;
+}
+
 // Маршрут для получения данных и перевода их в CSV
 app.get('/get-data', async (req, res) => {
   const { category, symbol, interval, start, end } = req.query;
   console.log(`Start: ${new Date(parseInt(start)).toISOString()}, End: ${new Date(parseInt(end)).toISOString()}`);
 
   try {
-    const response = await axios.get('https://api.bybit.com/v5/market/kline', {
-      params: {
-        category,
-        symbol,
-        interval,
-        start,
-        end
-      }
-    });
-
-    if (response.data.result && response.data.result.list) {
-      const csv = convertToCsv(response.data);
-      res.setHeader('Content-Type', 'text/csv');
-      res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
-      res.send(csv);
-    } else {
-      throw new Error('No data in response from Bybit API');
-    }
+    const params = { category, symbol, interval, start, end };
+    const allData = await fetchAllData(params);
+    const csv = convertToCsv(allData);
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
+    res.send(csv);
   } catch (error) {
     console.error('Error fetching data from Bybit API:', error.message);
     res.status(500).json({ error: `Error fetching data from Bybit API: ${error.message}` });
